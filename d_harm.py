b5f392b7a53ebffba3f4e1bc763c1db5ddf00cf0
ClaraShade <grzebyk.monika@gmail.com>
2021-02-18 20:55:31 +0100

    Check if user is already in database

--- d_harm.py
+++ d_harm.py
@@ -1,11 +1,24 @@
 import sqlite3
 con = sqlite3.connect('harmonogram.db')
 c = con.cursor()
-c.execute('CREATE TABLE users (id int, username str, password str)')
-c.execute('CREATE TABLE tasks (id int, fk int, time str, task str)')
-your_username = input("Wpisz nazwę użytkownika: ")
-your_password = input("Wpisz hasło: ")
-# if your_username jest w bazie
+# create database if not present
+# c.execute('CREATE TABLE users (user_id int, user_name str, password str)')
+# c.execute('CREATE TABLE tasks (task_id int, user_id int, time str, task str)')
+your_username = input(str("Type your login: "))
+u = (your_username,)
+c.execute('SELECT user_id FROM users WHERE user_name = ?', u)
+found_user = c.fetchall()
+if found_user == []:
+    print('O, hello, first time?')
+# def add(?,?,?):
+# input("Wpisz hasło: ")
+# id = len()
+# id = id+1
+# c.execute('INSERT INTO tasks(id, fk, time, task) VALUES (?, ?, ?)', (id, str(time), str(task)))
+
+#
+# your_password = input(str(("Wpisz hasło: "))
+
 # your_password = input("Wpisz hasło: ")
 # if password =  
 # if your_username nie jest w bazie
@@ -16,6 +29,10 @@ your_password = input("Wpisz hasło: ")
 # print("Ta nazwa jest zajęta. Wpisz inną nazwę")
 # your_password = input("Wpisz hasło: ")
 # our_password_check = input("Wpisz ponownie hasło: ")
-# c.execute('INSERT INTO users(id, username, password) VALUES (?, ?, ?)', (1, str(your_username), str(your_password)))
+# c.execute('INSERT INTO users(id, username, password) VALUES (?, ?, ?)', (increment, str(your_username), str(your_password)))
+# logowanie():
+#   x
+#   return fk
+#
 con.commit()
 con.close()
