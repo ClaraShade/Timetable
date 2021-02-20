0d8051edfbd3abadbe44cb00ec7e32d2bc311e1c
ClaraShade <grzebyk.monika@gmail.com>
2021-02-20 14:33:57 +0100

    Log-in completed

--- d_harm.py
+++ d_harm.py
@@ -4,11 +4,11 @@ c = con.cursor()
 # create database if not present
 # c.execute('CREATE TABLE users (user_id int, user_name str, password str)')
 # c.execute('CREATE TABLE tasks (task_id int, user_id int, time str, task str)')
-your_username = input(str("Hi, what is your name?: "))
+your_username = input(str("Hi, what is your name? "))
 u = (your_username,)
 c.execute('SELECT user_name FROM users WHERE user_name = ?', u)
 found_user = c.fetchall()
-if found_user == []:
+while found_user == []:
     print('Hello there, first time? \n'
     'Do you want to create new user?')
     add_user = input(str('Type "y", if you want to create new user. Type "n", if you want to type another name '))
@@ -20,29 +20,27 @@ if found_user == []:
         your_id = (len(count_user)) +1
         c.execute('INSERT INTO users VALUES (?,?,?)', (your_id, your_username, your_password))
         print('Congratulations, ' +your_username+ ', now you can create your harmonogram!')
+        raw_input()
     else:
         your_username = input(str("Type your name: "))
         u = (your_username,)
         c.execute('SELECT user_name FROM users WHERE user_name = ?', u)
         found_user = c.fetchall()
-        print(found_user)
-        #c.execute('INSERT INTO users(user_id, user_name, password) VALUES (?, ?, ?)', (increment, str(your_username), str(your_pa
-else:
-    print('Hello, '+ your_username)
+print('Hello, '+ your_username)
+your_password = input(str("Type your password: "))
+p = (your_password,)
+c.execute('SELECT password FROM users WHERE password = ?', p)
+found_password = c.fetchall()
+listed_p = [p]
+while found_password != listed_p:
+    print('Wrong password, type again')
     your_password = input(str("Type your password: "))
     p = (your_password,)
     c.execute('SELECT password FROM users WHERE password = ?', p)
     found_password = c.fetchall()
     listed_p = [p]
-    while found_password != listed_p:
-        print('Wrong password, type again')
-        your_password = input(str("Type your password: "))
-        p = (your_password,)
-        c.execute('SELECT password FROM users WHERE password = ?', p)
-        found_password = c.fetchall()
-        listed_p = [p]
-    print('Welcome to your harmonogram, ' + your_username)
-
+print('Welcome to your harmonogram, ' + your_username)
+raw_input()
 # def add(?,?,?):
 # input("Wpisz hasło: ")
 # id = len()
