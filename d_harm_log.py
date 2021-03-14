7c896b68a57f1eb60bda7dd557af285f639c9c63
ClaraShade <grzebyk.monika@gmail.com>
2021-03-14 18:05:50 +0100

    Adding tasks

--- d_harm_log.py
+++ d_harm_log.py
@@ -1,54 +1,44 @@
 import sqlite3
-
 con = sqlite3.connect('timetable.db')
 c = con.cursor()
 # create database if not present
-# c.execute('CREATE TABLE users (user_id INT AUTO_INCREMENT PRIMARY KEY, user_name str, password str)')
-# c.execute('CREATE TABLE tasks (task_id int AUTO_INCREMENT PRIMARY KEY, user_id int, time str, task str, FOREIGN KEY(user_id) REFERENCES users(user_id))')
+# c.execute('CREATE TABLE users (user_id int, user_name str, password str)')
+# c.execute('CREATE TABLE tasks (task_id int, user_id int, time str, task str)')
 your_username = input(str("Hi, what is your name? "))
 u = (your_username,)
 c.execute('SELECT user_name FROM users WHERE user_name = ?', u)
 found_user = c.fetchall()
 while not found_user:
     print('Hello there, first time? \n'
-          'Do you want to create new user?')
+    'Do you want to create new user?')
     add_user = input(str('Type "y", if you want to create new user. Type "n", if you want to type another name '))
     if add_user == 'y':
-        your_username = input(str("Type your name: "), )
-        your_password = input(str("Type your password: "), )
-        c.execute('INSERT INTO users VALUES (?,?,?)', (1, your_username, your_password))
-        print('Congratulations, ' + your_username + ', now you can create your timetable!')
-        x = input()
+        your_password = input(str("Type your password: "),)
+        c.execute('Select user_id COUNT FROM users')
+        count_user = c.fetchall()
+        your_id = (len(count_user)) +1
+        c.execute('INSERT INTO users VALUES (?,?,?)', (your_id, your_username, your_password))
+        print('Congratulations, ' +your_username+ ', now you can create your timetable!')
+        found_user = your_username
     else:
         your_username = input(str("Type your name: "))
         u = (your_username,)
         c.execute('SELECT user_name FROM users WHERE user_name = ?', u)
         found_user = c.fetchall()
-print('Hello, ' + your_username)
-your_password = input(str("Type your password: "))
-p = (your_password,)
-c.execute('SELECT password FROM users WHERE password = ?', p)
-found_password = c.fetchall()
-listed_p = [p]
-while found_password != listed_p:
-    print('Wrong password, type again')
-    your_password = input(str("Type your password: "))
-    p = (your_password,)
-    c.execute('SELECT password FROM users WHERE password = ?', p)
-    found_password = c.fetchall()
-    listed_p = [p]
-print('Welcome to your timetable, ' + your_username)
-
-
-def get_user_id():
-    get_login = (your_username,)
-    c.execute('SELECT user_id FROM users WHERE user_name = ?', get_login)
-    get_id = c.fetchall()
-    return get_id[0][0]
-
-
-my_id = get_user_id()
-
+        print('Hello, '+ your_username)
+        your_password = input(str("Type your password: "))
+        p = (your_password,)
+        c.execute('SELECT password FROM users WHERE password = ?', p)
+        found_password = c.fetchall()
+        listed_p = [p]
+        while found_password != listed_p:
+            print('Wrong password, type again')
+            your_password = input(str("Type your password: "))
+            p = (your_password,)
+            c.execute('SELECT password FROM users WHERE password = ?', p)
+            found_password = c.fetchall()
+            listed_p = [p]
+print('Welcome to your harmonogram, ' + your_username)
 x = input()
 con.commit()
 con.close()
