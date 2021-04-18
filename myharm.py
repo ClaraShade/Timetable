4479860f1ca24e29e1b1ec9bd76ebdd900deaf88
ClaraShade <grzebyk.monika@gmail.com>
2021-04-18 16:30:50 +0200

    switch_tasks added

--- myharm.py
+++ myharm.py
@@ -57,7 +57,7 @@ myID = get_user_id()
 
 def add_task():
     day = str(input("Please type day in format yyyy-mm-dd: "))
-    hour = str(input("Please type hour in 24-hour format: "))
+    hour = str(input("Please type time in 24-hour format: "))
     task = str(input("Please type task "))
     mydate = (day + ' ' + hour)
     c.execute('INSERT INTO tasks (userID, time, task) VALUES (?,?,?)', (myID[0], mydate, task))
@@ -67,7 +67,6 @@ def del_task():
     toremove = str(input("Which task do you want to remove? "))
     c.execute("DELETE FROM tasks WHERE userID = ? AND task =?", (myID[0], toremove))
 
-# del_task()
 
 def show_tasks():
     # global whatifound if needed in future
@@ -80,17 +79,28 @@ def show_tasks():
         c.execute('SELECT t.time, t.task FROM tasks t, users u WHERE t.userID = u.userID AND u.userID = ? AND t.time like ?',
                   (myID[0], daytoday + '%'))
         whatifound = c.fetchall()
-        print(whatifound)
+        if whatifound:
+            print(whatifound)
+        else:
+            print("You have no tasks today")
+
     elif toshow == 'week':
-        daytoday = datetime.date.today()
-        print(datetime.date.isoweekday(daytoday))
+        daytoday = date.today()
+        weekofyear = daytoday.isocalendar()[1]
+        #weektoday = date.isoweekday(daytoday)
+        print(weekofyear)
+        c.execute(
+            'SELECT t.time, t.task FROM tasks t, users u WHERE t.userID = u.userID AND u.userID = ? AND datetime.fromisoformat(t.time) = ?',
+            (myID[0], 15))
+        whatifound = c.fetchall()
+        if whatifound:
+            print(whatifound[0][0] + ': ' +whatifound[0][1])
+        else:
+            print("You have no tasks this week")
     elif toshow == 'month':
         somemonth = int(input('Type month 1-12: '))
         daytoday = date.today()
         yeartoday = daytoday.year
-        # >>> datetime(2019, 5, 18, 15, 17, 8, 132263).isoformat()
-        # '2019-05-18T15:17:08.132263'
-        # tu się nie baw i lepiej datetimem chyba
         while somemonth > 12:
             somemonth = int(input('Type month 1-12: '))
         if somemonth > 9:
@@ -122,9 +132,27 @@ def show_tasks():
         else:
             print("You have no tasks at this date")
 
-#add_task()
+def switch_tasks():
+    day1 = str(input("Please type the date of the first task in format yyyy-mm-dd: "))
+    hour1 = str(input("Please type the time of the first task in 24-hour format: "))
+    task1 = str(input("Please type the name of the first task "))
+    firsttask = (myID[0], (day1 + ' ' + hour1), task1)
+    print(firsttask)
+    c.execute("DELETE FROM tasks WHERE userID = ? AND time =? AND task =?", (myID[0], (day1 + ' ' + hour1), task1))
+    day2 = str(input("Please type the date of the second task in format yyyy-mm-dd: "))
+    hour2 = str(input("Please type the time of the second task in 24-hour format: "))
+    task2 = str(input("Please type the name of the second task "))
+    secondtask = (myID[0], (day2 + ' ' + hour2), task2)
+    print(secondtask)
+    c.execute("DELETE FROM tasks WHERE userID = ? AND time =? AND task =?", (myID[0], (day2 + ' ' + hour2), task2))
+    c.execute('INSERT INTO tasks (userID, time, task) VALUES (?,?,?)', (myID[0], (day1 + ' ' + hour1), task2))
+    c.execute('INSERT INTO tasks (userID, time, task) VALUES (?,?,?)', (myID[0], (day2 + ' ' + hour2), task1))
 
-show_tasks()
+
+#add_task()
+#del_task()
+#show_tasks()
+switch_tasks()
 
 #def find_task():
 #mytask = str(input('Type task name to find: '))
